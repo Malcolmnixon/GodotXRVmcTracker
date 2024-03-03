a7d5e86b80d5287430bb5fbc564c7ac8e10f0d49
Malcolm Nixon <Malcolm.nixon@gmail.com>
2024-03-03 00:03:32 -0500

    Apply root position from current hips.

--- addons/godot_vmc_tracker/vmc_tracker.gd
+++ addons/godot_vmc_tracker/vmc_tracker.gd
@@ -206,13 +206,6 @@ func _process_joints() -> void:
 		_abs_positions[joint] = pos
 		_abs_rotations[joint] = rot
 
-	# Calculate and set the root joint under the hips
-	var root := _body_tracker.get_joint_transform(XRBodyTracker.JOINT_HIPS)
-	root.basis = Basis.IDENTITY
-	root.origin = root.origin.slide(Vector3.UP)
-	_body_tracker.set_joint_transform(XRBodyTracker.JOINT_ROOT, root)
-	_body_tracker.set_joint_flags(XRBodyTracker.JOINT_ROOT, JOINT_TRACKING)
-
 	# Apply to the XRBodyTracker
 	for joint in VMCBody.JOINT_MAPPING:
 		var body : XRBodyTracker.Joint = joint["body"]
@@ -228,6 +221,13 @@ func _process_joints() -> void:
 
 		# Set the joint flags
 		_body_tracker.set_joint_flags(body, JOINT_TRACKING)
+
+	# Calculate and set the root joint under the hips
+	var root := _body_tracker.get_joint_transform(XRBodyTracker.JOINT_HIPS)
+	root.basis = Basis.IDENTITY
+	root.origin = root.origin.slide(Vector3.UP)
+	_body_tracker.set_joint_transform(XRBodyTracker.JOINT_ROOT, root)
+	_body_tracker.set_joint_flags(XRBodyTracker.JOINT_ROOT, JOINT_TRACKING)
 
 	# Indicate we are tracking the body
 	_body_tracker.body_flags = BODY_TRACKING
